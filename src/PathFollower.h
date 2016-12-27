5ff5d8f348b0a19ec582ab2b88553f97d521dc7e
Ohohcakester <ohohgit@gmail.com>
2016-12-27 11:24:58 +0800

    Pathfollower methods to check if object has reached destination

--- src/PathFollower.h
+++ src/PathFollower.h
@@ -2,9 +2,9 @@
 #define _PATH_FOLLOWER_H_
 
 #include "IGameObject.h"
+#include "GameMap.h"
 #include "Pathfinding/PathfindingDataTypes.h"
 #include <vector>
-class GameMap;
 
 class PathFollower: public IGameObject {
 protected:
@@ -16,6 +16,19 @@ protected:
     void followPath(const GameMap& gameMap);
     void goToCoordinate(int destX, int destY, const GameMap& gameMap);
     void goToPosition(int destX, int destY, const GameMap& gameMap);
+
+public:
+    inline bool stoppedMoving() const {return remainingPath.size() == 0;}
+
+    inline bool reachedDestination(GridVertex destination, const GameMap& gameMap) const {
+        int px = x, py = y;
+        gameMap.getNearestGridCoordinate(px, py);
+        return (px == destination.x && py == destination.y);
+    }
+
+    inline bool stoppedAndReachedDestination(GridVertex destination, const GameMap& gameMap) const {
+        return stoppedMoving() && reachedDestination(destination, gameMap);
+    }
 };
 
 
